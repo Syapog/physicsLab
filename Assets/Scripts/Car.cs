a379dbd3d282cc8b7a5d7c03c75f6372b2b97c7e
Syapog <1anton.draganov@mail.ru>
2024-09-02 16:36:33 +0500

    create

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI SText;
+    [SerializeField] private TextMeshProUGUI VText;
+    [SerializeField] private TextMeshProUGUI TText;
+    [SerializeField] private TextMeshProUGUI BrakeTText;
+    [SerializeField] private TextMeshProUGUI GasTText;
+
+    [SerializeField] private bool IsTest;
+
+    private float S = 0;
+    private float V0 = 0, V = 0;
+    private float T = 0, gasT = 0, brakeT = 0;
+    private float N, m, g, my;
+
+    bool timeIsCounted = false;
+
+    private void Start()
+    {
+
+        N = 58800f;
+        m = 1300000f;
+        g = 9.8f;
+        my = 0.8f;
+    }
+
+    private void FixedUpdate()
+    {
+        if(timeIsCounted)
+        {
+            T += Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.W))
+        {
+
+            V0 = V;
+            if (!timeIsCounted)
+                timeIsCounted = true;
+            else
+                gasT += Time.deltaTime;
+            V = Mathf.Sqrt(V0 * V0 + 2 * N * gasT / m);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            if (timeIsCounted)
+                brakeT += Time.deltaTime;
+            V -= my * g * Time.deltaTime;
+            if (V < 0)
+                V = 0;
+        }
+        else
+        {
+            gasT = 0;
+            brakeT = 0;
+        }
+        
+        transform.Translate(0, 0, V * Time.deltaTime);
+        S += V * Time.deltaTime;
+
+        SText.text = "S = " + S;
+        VText.text = "V = " + V;
+        TText.text = "T = " + T;
+        if(IsTest)
+        {
+            if (V > 0)
+                BrakeTText.text = "brakeT = " + brakeT;
+            if (V < 27.8f)
+                GasTText.text = "gasT = " + gasT;
+        }
+        else
+        {
+            BrakeTText.text = "brakeT = " + brakeT;
+            GasTText.text = "gasT = " + gasT;
+        }
+    }
+}
---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    [SerializeField] private GameObject RoadPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Car car;
+        if(other.TryGetComponent<Car>(out car))
+        {
+            Instantiate(RoadPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z + 6f), Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
